bbaa24746d287927fed9808a8e27881be67b7d5c
Brendan Rosa <brendan.rosa@gmail.com>
2020-04-09 14:31:27 -0400

    First stab at refactoring/abstraction exercise.

--- src/dev/brenr/Main.java
+++ src/dev/brenr/Main.java
@@ -5,13 +5,58 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(final String[] args) {
-        final Employee employee = new Employee(50_000, 20);
-        Employee.printNumberOfEmployees();
-        final int wage = employee.calculateWage();
-        System.out.println(wage);
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance();
 
-        NumberFormat
+    public static void main(final String[] args) {
+        final int principal = promptForInt("Principal");
+        final float rate = promptForFloat("Annual Interest Rate");
+        final byte years = promptForByte("Period (Years)");
+
+        final Loan loan = new Loan(principal, rate, years);
+        final MortgageCalculator mortgageCalculator = new MortgageCalculator((loan));
+
+        printMortgageSection(mortgageCalculator);
+        printPaymentScheduleSection(mortgageCalculator);
+    }
+
+    private static int promptForInt(final String prompt) {
+        promptMessage(prompt);
+        return scanner.nextInt();
+    }
+
+    private static float promptForFloat(final String prompt) {
+        promptMessage(prompt);
+        return scanner.nextFloat();
+    }
+
+    private static byte promptForByte(final String prompt) {
+        promptMessage(prompt);
+        return scanner.nextByte();
+    }
+
+    private static void promptMessage(final String string) {
+        System.out.print(string + ": ");
+    }
+
+    private static void printMortgageSection(final MortgageCalculator mc) {
+        System.out.println("\nMORTGAGE\n--------");
+        System.out.println("Monthly Payments: " +
+                currencyFormatter.format(mc.calculateMortgage()));
+    }
+
+    private static void printPaymentScheduleSection(final MortgageCalculator mc) {
+        final double[] balances = mc.getPaymentSchedule();
+        System.out.println("\nPAYMENT SCHEDULE\n----------------");
+        System.out.println(getPaymentScheduleString(balances));
+    }
+
+    private static final String getPaymentScheduleString(final double[] balances) {
+        StringBuilder sb = new StringBuilder();
+        for(double balance : balances) {
+            sb.append(currencyFormatter.format(balance) + "\n");
+        }
+        return sb.toString();
     }
 
 }
--- src/dev/brenr/MortgageCalculator.java
+++ src/dev/brenr/MortgageCalculator.java
@@ -1,130 +1,40 @@
 package dev.brenr;
 
-import java.text.NumberFormat;
-import java.util.Scanner;
-
 public class MortgageCalculator {
 
-    private static final byte MONTHS_IN_YEAR = 12;
-    private static final byte PERCENT = 100;
+    private Loan loan;
 
-    private static final Scanner scanner = new Scanner(System.in);
-    private static final NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance();
-
-    public static void main(String[] args) {
-
-        final int principalAmount = (int)readNumber(
-                "Principal: ",
-                1_000,
-                1_000_000);
-        final float annualInterest = (int)readNumber(
-                "Annual Interest Rate: ",
-                1,
-                30);
-        final byte years = (byte)readNumber(
-                "Period (Years): ",
-                1,
-                30);
-
-
-        // Print mortgage section
-        printMortgageSection(principalAmount, annualInterest, years);
-
-        // Print payment schedule section
-        printPaymentScheduleSection(principalAmount, annualInterest, years);
+    public MortgageCalculator(final Loan loan) {
+        this.loan = loan;
     }
 
-    private static void printPaymentScheduleSection(
-            final int principalAmount,
-            final float annualInterest,
-            final byte years) {
-
-        final double[] balances = getPaymentSchedule(principalAmount, annualInterest, years);
-        System.out.println("\nPAYMENT SCHEDULE\n" + "----------------" +
-                           "\n" + getPaymentScheduleString(balances));
-    }
-
-    private static void printMortgageSection(
-            final int principalAmount,
-            final float annualInterest,
-            final byte years) {
-
-        final double mortgage = calculateMortgage(principalAmount, annualInterest, years);
-        System.out.println("\nMORTGAGE\n" + "--------" +
-                           "\nMonthly Payments: " + currencyFormatter.format(mortgage));
-    }
-
-    private static double calculateMortgage(
-            final int principalAmount,
-            final float annualInterest,
-            final byte years) {
-
-        final double monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
-        final short payments = (short)(years * MONTHS_IN_YEAR);
-        final double onePlusRatePowN = Math.pow(1 + monthlyInterest, payments);
-
-        return principalAmount *
-                ((monthlyInterest * onePlusRatePowN)
+    public double calculateMortgage() {
+        final double onePlusRatePowN = Math.pow(1 + loan.getRate(), loan.getPeriod());
+        return loan.getPrincipal() *
+                ((loan.getRate() * onePlusRatePowN)
                 / (onePlusRatePowN - 1));
     }
 
-    private static double calculateRemainingBalance(
-            final int principalAmount,
-            final double annualInterest,
-            final short years,
-            final short paymentsMadeToDate) {
-
-        final double monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
-        final short principalPayments = (short)(years * MONTHS_IN_YEAR);
-
-        final double rate = 1 + monthlyInterest;
-        final double principalInterest = Math.pow(rate, principalPayments);
-        final double paidInterest = Math.pow(rate, paymentsMadeToDate);
-
-        return principalAmount *
-                (principalInterest - paidInterest)
-                / (principalInterest - 1);
+    private double calculateRemainingBalance() {
+        return loan.getPrincipal() *
+                (getPrincipalInterest() - getPaidInterest())
+                / (getPrincipalInterest() - 1);
     }
 
-    private static double[] getPaymentSchedule(
-            final int principal,
-            final double annualInterest,
-            final byte years) {
+    private double getPrincipalInterest() {
+        return Math.pow(1 + loan.getRate(), loan.getPeriod());
+    }
 
-        final short principalPayments = (short) (years * MONTHS_IN_YEAR);
-        final double[] balances = new double[principalPayments];
+    private double getPaidInterest() {
+        return Math.pow(1 + loan.getRate(), loan.getPaymentsMade());
+    }
 
-        for(short i = 0; i < balances.length; i++) {
-            balances[i] = calculateRemainingBalance(
-                    principal,
-                    annualInterest,
-                    years,
-                    ((short)(i + 1)));
+    public double[] getPaymentSchedule() {
+        final double[] balances = new double[loan.getPeriod()];
+        for(short i = 0; i < balances.length; loan.setPaymentsMade(++i)) {
+            balances[i] = calculateRemainingBalance();
         }
-
         return balances;
-    }
-
-    private static final String getPaymentScheduleString(final double[] balances) {
-        StringBuilder sb = new StringBuilder();
-        for(double balance : balances) {
-            sb.append(currencyFormatter.format(balance) + "\n");
-        }
-
-        return sb.toString();
-    }
-
-    private static double readNumber(String prompt, double min, double max) {
-        double value;
-
-        while(true) {
-            System.out.print(prompt);
-            value = scanner.nextDouble();
-            if(value >= min && value <= max) {
-                return value;
-            }
-            System.out.println("Enter a value between " + min + " and " + max);
-        }
     }
 
 }
